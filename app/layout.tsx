a92a901ccbdd0b3eab84a62b6874a42da1eba868
Rodrigo Tallar <89488066+rtallarr@users.noreply.github.com>
2026-08-15 11:14:35 -0400

    fix: navbar import in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
-import NavBar from "@/components/navbar"
+import NavBar from "@/components/navBar"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// @ts-expect-error
 import "./globals.css";
 
 const geistSans = Geist({
