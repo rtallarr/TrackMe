4c4913e07575e7f93d99a11746523840d0923f67
Rodrigo Tallar <89488066+rtallarr@users.noreply.github.com>
2026-08-15 11:19:39 -0400

    fix: remove ts-ignore-error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Toaster } from "@/components/ui/sonner"
 import NavBar from "@/components/navBar"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-expect-error
 import "./globals.css";
 
 const geistSans = Geist({
