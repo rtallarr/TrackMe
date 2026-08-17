bc2ab7cd41bc483820157073a10ea3a819a8d0a1
Rodrigo Tallar <89488066+rtallarr@users.noreply.github.com>
2026-08-17 01:50:25 -0400

    fix: capitalization change to match file name uploaded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
-import NavBar from "@/components/navbar"
+import NavBar from "@/components/navBar"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
